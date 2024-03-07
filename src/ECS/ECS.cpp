1d5d91628a8a2eacc73249fea6144e2a699b694f
vchakoshy <vchakoshy@gmail.com>
2024-03-07 15:20:53 +0330

    operator overloading on entities

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -15,7 +15,7 @@ void System::RemoveEntityFromSystem(Entity entity)
 {
     entities.erase(
         std::remove_if(entities.begin(), entities.end(), [&entity](Entity other)
-                       { return entity.GetId() == other.GetId(); }),
+                       { return entity == other; }),
         entities.end());
 }
 
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -34,6 +34,10 @@ public:
     {
     }
     int GetId() const;
+
+    Entity &operator=(const Entity &other) = default;
+    bool operator==(const Entity &other) const { return id == other.id; };
+    bool operator!=(const Entity &other) const { return id != other.id; };
 };
 
 class System

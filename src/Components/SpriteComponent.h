3fbada96ce9f25d53adbf434d90fb53d97204691
vahid chakoshy <vchakoshy@gmail.com>
2024-03-16 01:26:37 +0330

    add sorting order

--- src/Components/SpriteComponent.h
+++ src/Components/SpriteComponent.h
@@ -10,13 +10,15 @@ struct SpriteComponent
     std::string assetId; 
     int width;
     int height;
+    int zindex;
     SDL_Rect srcRect;
 
-    SpriteComponent(std::string assetId = "", int width = 0, int height = 0, int srcRectX = 0, int srcRectY = 0)
+    SpriteComponent(std::string assetId = "", int width = 0, int height = 0, int zindex = 0, int srcRectX = 0, int srcRectY = 0)
     {
         this->assetId = assetId;
         this->height = height;
         this->width = width;
+        this->zindex = zindex;
         this->srcRect = {srcRectX, srcRectY, width, height};
     }
 };
--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -92,7 +92,7 @@ void Game::LoadLevel(int level){
 
             Entity tile = registry->CreateEntity();
             tile.AddComponent<TransformComponent>(glm::vec2(x * (tileScale * tileSize), y * (tileScale * tileSize)), glm::vec2(tileScale, tileScale), 0.0);
-            tile.AddComponent<SpriteComponent>("tilemap-image", tileSize, tileSize, srcRectX,srcRectY);
+            tile.AddComponent<SpriteComponent>("tilemap-image", tileSize, tileSize, 0, srcRectX,srcRectY);
         }
     }
 
@@ -103,13 +103,13 @@ void Game::LoadLevel(int level){
     tank.AddComponent<TransformComponent>(
         glm::vec2(10.0, 10.0), glm::vec2(0.0, 0.0), 0.0);
     tank.AddComponent<RigidBodyComponent>(glm::vec2(40.0, 0.0));
-    tank.AddComponent<SpriteComponent>( "tank-image", 32, 32);
+    tank.AddComponent<SpriteComponent>( "tank-image", 32, 32, 2);
 
     Entity truck = registry->CreateEntity();
     truck.AddComponent<TransformComponent>(
         glm::vec2(50.0, 100.0), glm::vec2(1.0, 1.0), 0.0);
     truck.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 50.0));
-    truck.AddComponent<SpriteComponent>("truck-image", 32, 32);
+    truck.AddComponent<SpriteComponent>("truck-image", 32, 32, 1);
 }
 
 void Game::Setup()

d6edc13ae06d62c9ce12ca0d7c24048c2bfd8745
vahid chakoshy <vchakoshy@gmail.com>
2024-03-16 01:04:45 +0330

    add tilemap

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "../Logger/Logger.h"
 #include "../Components/TransformCompnent.h";
 #include "../Components/RigidBodyComponent.h";
@@ -65,13 +66,38 @@ void Game::Initialize()
 glm::vec2 playerPosition;
 glm::vec2 playerVelocity;
 
-void Game::Setup()
-{
+void Game::LoadLevel(int level){
     registry->AddSystem<MovementSystem>();
     registry->AddSystem<RenderSystem>();
 
     assetStore->AddTexture(renderer, "tank-image", "./assets/images/tank-panther-right.png");
     assetStore->AddTexture(renderer, "truck-image", "./assets/images/truck-ford-right.png");
+    assetStore->AddTexture(renderer, "tilemap-image", "./assets/tilemaps/jungle.png");
+
+    int tileSize = 32;
+    double tileScale = 2.0;
+    int mapNumCols = 25;
+    int mapNumRows = 20;
+    std::fstream mapFile;
+    mapFile.open("./assets/tilemaps/jungle.map");
+
+    for (int y = 0; y<mapNumRows;y++){
+        for (int x = 0; x< mapNumCols; x++){
+            char ch;
+            mapFile.get(ch);
+            int srcRectY = std::atoi(&ch) * tileSize;
+            mapFile.get(ch);
+            int srcRectX = std::atoi(&ch) *tileSize;
+            mapFile.ignore();
+
+            Entity tile = registry->CreateEntity();
+            tile.AddComponent<TransformComponent>(glm::vec2(x * (tileScale * tileSize), y * (tileScale * tileSize)), glm::vec2(tileScale, tileScale), 0.0);
+            tile.AddComponent<SpriteComponent>("tilemap-image", tileSize, tileSize, srcRectX,srcRectY);
+        }
+    }
+
+    mapFile.close();
+
 
     Entity tank = registry->CreateEntity();
     tank.AddComponent<TransformComponent>(
@@ -84,6 +110,11 @@ void Game::Setup()
         glm::vec2(50.0, 100.0), glm::vec2(1.0, 1.0), 0.0);
     truck.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 50.0));
     truck.AddComponent<SpriteComponent>("truck-image", 32, 32);
+}
+
+void Game::Setup()
+{
+    LoadLevel(1);
 }
 
 void Game::Update()
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -22,6 +22,7 @@ public:
     ~Game();
     void Initialize();
     void Run();
+    void LoadLevel(int level);
     void Setup();
     void ProcessInput();
     void Update();
